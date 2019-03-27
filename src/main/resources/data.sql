e8ca42e5cd2494cf4e6c35de4652e921a96392c7
Pooya D. GOhardani <p.deldar@tax.gov.ir>
2019-03-27 11:14:49 +0430

    Initializing data add by data.sql in the root of resources.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (category_id, name) VALUES (1, 'America');
+INSERT INTO category (category_id, name) VALUES (2, 'Italian');
+INSERT INTO category (category_id, name) VALUES (3, 'Mexican');
+INSERT INTO category (category_id, name) VALUES (4, 'Fast Food');
+INSERT INTO unit_of_measure (id, unit) VALUES (1, 'Teaspoon');
+INSERT INTO unit_of_measure (id, unit) VALUES (2, 'Tablespoon');
+INSERT INTO unit_of_measure (id, unit) VALUES (3, 'Cup');
+INSERT INTO unit_of_measure (id, unit) VALUES (4, 'Pinch');
+INSERT INTO unit_of_measure (id, unit) VALUES (5, 'Ounce');
