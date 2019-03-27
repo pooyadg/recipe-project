3f2fa19828024939dc41ab1715bb8a44fcbbd004
Pooya D. GOhardani <p.deldar@tax.gov.ir>
2019-03-27 10:51:05 +0430

    Crud repositories added.

---
+++ src/main/java/com/example/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.example.recipe.repositories;
+
+import com.example.recipe.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
