415b03abf66f3991c1e0fc0db1a6a4bcb4a07e87
Pooya D. GOhardani <p.deldar@tax.gov.ir>
2019-04-13 18:22:06 +0430

    Persisting image is done.

--- src/main/java/com/example/recipe/controllers/ImageController.java
+++ src/main/java/com/example/recipe/controllers/ImageController.java
@@ -1,22 +1,41 @@
 package com.example.recipe.controllers;
 
+import com.example.recipe.services.ImageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Controller
 public class ImageController {
 
+    private ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
 
     @GetMapping("recipe/{id}/image")
-    public String getImageView(@PathVariable String id, Model model) {
+    public String showUploadForm(@PathVariable String id, Model model) {
 
         model.addAttribute("recipeId", id);
 
         return "/recipe/imageform";
+    }
+
+    @PostMapping("recipe/{id}/image")
+    public String handleImagePost(@PathVariable String id, @RequestParam("imageFile") MultipartFile file) {
+
+        log.debug("In get new image");
+
+        imageService.saveImageFile(Long.parseLong(id), file);
+
+        return "redirect:/recipe/" + id + "/details";
     }
 
 }
--- src/test/java/com/example/recipe/controllers/ImageControllerTest.java
+++ src/test/java/com/example/recipe/controllers/ImageControllerTest.java
@@ -1,15 +1,21 @@
 package com.example.recipe.controllers;
 
+import com.example.recipe.services.ImageService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class ImageControllerTest {
@@ -19,26 +25,50 @@ public class ImageControllerTest {
     @Mock
     private Model model;
 
+    @Mock
+    private ImageService imageService;
+
     private MockMvc mockMvc;
+    public static final Long SAMPLE_ID = 1L;
 
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        imageController = new ImageController();
+        imageController = new ImageController(imageService);
         mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
     }
 
     @Test
     public void testGetImageView() throws Exception {
         //given
-        Long sampleId = 1L;
 
         //when then
-        mockMvc.perform(get("/recipe/" + sampleId.toString() + "/image"))
+        mockMvc.perform(get("/recipe/" + SAMPLE_ID + "/image"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.model().attributeExists("recipeId"))
                 .andExpect(MockMvcResultMatchers.view().name("/recipe/imageform"));
+
+    }
+
+    @Test
+    public void testHandleImagePost() throws Exception {
+        //given
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "imageFile",
+                "test.text",
+                "text/plain",
+                "This is a sample content".getBytes());
+
+        //when //then
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/recipe/" + SAMPLE_ID + "/image")
+                .file(mockMultipartFile))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.header().string("Location", "/recipe/" + SAMPLE_ID + "/details"))
+        ;
+
+        Mockito.verify(imageService, Mockito.times(1)).saveImageFile(anyLong(), any());
+
 
     }
 }
